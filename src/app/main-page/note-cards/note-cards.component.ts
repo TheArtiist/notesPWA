c33fec69b9a812a5a3bd4f255e8ed3be32b26b2e
Szűcs Bence <h378168@stud.u-szeged.hu>
2025-10-08 22:33:29 +0200

    notes-cards-component init, a bit styling

---
+++ src/app/main-page/note-cards/note-cards.component.ts
@@ -0,0 +1,15 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-note-cards',
+  standalone: true,
+  imports: [],
+  templateUrl: './note-cards.component.html',
+  styleUrl: './note-cards.component.scss'
+})
+export class NoteCardsComponent {
+  @Input() title!: string;
+  @Input() content!: string;
+  @Input() date!: string; //Last update date
+
+}
