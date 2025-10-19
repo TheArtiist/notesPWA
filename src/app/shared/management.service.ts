7687386771c534adfec28bf07d8a641e848e8ea1
Szűcs Bence <h378168@stud.u-szeged.hu>
2025-10-19 23:30:35 +0200

    ManagementService init

---
+++ src/app/shared/management.service.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { NoteCardsComponent } from '../main-page/note-cards/note-cards.component';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ManagementService {
+  public readonly notes: NoteCardsComponent[] = [];
+  constructor() { }
+
+  public createNote(title: string, content: string, date: string){
+
+  }
+}
